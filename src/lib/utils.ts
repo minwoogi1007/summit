ca2182e71fc04068a9ced71cdfc60e73fcbab68b
minwo <minwoogi@gmail.com>
2025-12-12 01:33:18 +0900

    Fix debounce type error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -74,7 +74,8 @@ export function truncateText(text: string, maxLength: number): string {
 }
 
 // 디바운스 함수
-export function debounce<T extends (...args: unknown[]) => unknown>(
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => any>(
   func: T,
   wait: number
 ): (...args: Parameters<T>) => void {
